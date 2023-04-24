2208e72f989fb7c259b686b4cbfe3124272122cb
Mario <cerdamario13@gmail.com>
2023-04-23 20:15:28 -0500

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <Box
+        m={2}
+        pt={3}
+        paddingX={5}
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+          'border': '1px solid'
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Typography variant='h2'>
+          Kaprekar constant
+        </Typography>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Kaprekar constant, or 6174, is a constant that arises when we take a 4-digit integer, form the largest and smallest numbers from its digits, and then subtract these two numbers. Continuing with this process of forming and subtracting, we will always arrive at the number 6174.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+        <p>Try it yourself</p>
+        <TextField
+          required
+          id="outlined-required"
+          label="Enter a Value"
+          defaultValue="6147"
+          type='number'
+        />
+        <Button variant="outlined">Outlined</Button>
+        
+      </Box>      
+    </>
   );
 }
 
