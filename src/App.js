2712b6948d1da70a00f6f4ad79d2675cbb061670
Mario Cerda <cerdamario13@gmail.com>
2023-05-08 20:26:33 -0500

    capitalized title

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ function App() {
         autoComplete="off"
       >
         <Typography variant='h2'>
-          Kaprekar constant
+          Kaprekar Constant
         </Typography>
         <p>
         Kaprekar constant, or 6174, is a constant that arises when we take a 4-digit integer, form the largest and smallest numbers from its digits, and then subtract these two numbers. Continuing with this process of forming and subtracting, we will always arrive at the number 6174.
