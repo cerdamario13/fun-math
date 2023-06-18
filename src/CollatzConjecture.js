bc2e2c55c7b03de96b35175a3a7a3e7de8eff8e1
Mario <cerdamario13@gmail.com>
2023-06-18 17:01:36 -0500

    working function collatz

--- src/CollatzConjecture.js
+++ src/CollatzConjecture.js
@@ -1,8 +1,46 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { useState } from 'react';
+import { Button, Stack, TextField, Typography } from '@mui/material';
 
 const CollatzConjecture = () => {
+  
+  const [numberVal, setNumberVal] = useState("");
+  
+  //Collatz Conjecture recursive function
+  const collatz_func = (value, operations = []) => {
+    
+    var valueNum = Number(value);
+    
+    //break the function if the value is 1
+    if (valueNum == 1) {
+      operations.push(value);
+      return operations;
+    }
+    
+    if (valueNum % 2 == 0) {
+      //Even result
+      operations.push(valueNum);
+      return collatz_func(valueNum / 2, operations);
+      
+    } else {
+      // Odd result
+      operations.push(valueNum);
+      return collatz_func( (3 * valueNum) + 1, operations );
+    }
+    
+  }
+  
+  const calculateValue = () => {
+    
+    var CollatzOps = collatz_func(numberVal);
+    console.log(CollatzOps);
+    
+  };
+  
+  const clearOperations = () => {
+    setNumberVal("");
+  }
   
   return (
     <>
@@ -25,6 +63,22 @@ const CollatzConjecture = () => {
         <p>
           The Collatz conjecture is one of the most famous unsolved problems in mathematics. The conjecture asks whether repeating two simple arithmetic operations will eventually transform every positive integer into 1. It concerns sequences of integers in which each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture is that these sequences always reach 1, no matter which positive integer is chosen to start the sequence.
         </p>
+        
+        <Stack
+          spacing={2} direction="row">
+          <TextField
+            InputLabelProps={{ shrink: true }}
+            id='inputNumber'
+            label="Enter a Value"
+            value={numberVal}
+            type='number'
+            onChange={(event) => setNumberVal(event.target.value)}
+          />
+          <Button variant='outlined' onClick={() => calculateValue()}>Calculate</Button>
+          <Button variant='outlined' onClick={() => clearOperations()}>Clear</Button>
+          
+        </Stack>
+        
       </Box>
     </>
   );
