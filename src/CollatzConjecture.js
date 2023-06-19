1aeebd3ae6be5ce2c833598592899f3fdf493510
Mario <cerdamario13@gmail.com>
2023-06-18 19:17:20 -0500

    info button with link

--- src/CollatzConjecture.js
+++ src/CollatzConjecture.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
-import { Alert, Button, Stack, TextField, Typography } from '@mui/material';
+import { Alert, Button, IconButton, Stack, TextField, Typography } from '@mui/material';
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts"
+import { InfoOutlined } from '@mui/icons-material';
 
 
 const CollatzConjecture = () => {
@@ -79,6 +80,10 @@ const CollatzConjecture = () => {
     setErrorStatus(false);
   }
   
+  const handleInfoClick = () => {
+    window.open("https://en.wikipedia.org/wiki/Collatz_conjecture", "_blank");
+  }
+  
   
   return (
     <>
@@ -95,9 +100,16 @@ const CollatzConjecture = () => {
         noValidate
         autoComplete="off"
       >
-        <Typography variant='h2'>
-          Collatz Conjecture
-        </Typography>
+        <Stack direction="row"spacing={2}>
+          <Typography variant='h2'>
+            Collatz Conjecture
+          </Typography>
+          
+          <IconButton onClick={handleInfoClick}>
+            <InfoOutlined />
+          </IconButton>          
+        </Stack>
+        
         <p>
           The Collatz conjecture is one of the most famous unsolved problems in mathematics. The conjecture asks whether repeating two simple arithmetic operations will eventually transform every positive integer into 1. It concerns sequences of integers in which each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture is that these sequences always reach 1, no matter which positive integer is chosen to start the sequence.
         </p>
