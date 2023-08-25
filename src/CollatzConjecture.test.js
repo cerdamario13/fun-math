57410231ec8091f91948004ac2b15b96f03364bc
Mario <cerdamario13@gmail.com>
2023-08-24 20:22:50 -0500

    starting added tests

--- src/CollatzConjecture.test.js
+++ src/CollatzConjecture.test.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { ReactDOM } from 'react';
-import { render, cleanup, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+
 import CollatzConjecture from "./CollatzConjecture";
 
-afterEach(cleanup)
+// afterEach(cleanup)
     
-it("Ensure that the main items are present", () => {
-    render(<CollatzConjecture />);
-    screen.getByText('Collatz Conjecture');
-    expect(screen.getByText('Collatz Conjecture')).toBeInTheDocument();
-    
-    expect(screen.getByRole('spinbutton')).toBeInTheDocument();
-    
-    expect(screen.getByRole('button', {name: "Plot"})).toBeInTheDocument();
-    
-    expect(screen.getByRole('button', {name: "Clear"})).toBeInTheDocument();
-});
+// test("Ensure that the main items are present", () => {
+//   render(<CollatzConjecture />);
+//   screen.getByText('Collatz Conjecture');
+//   expect(screen.getByText('Collatz Conjecture')).toBeInTheDocument();
+  
+//   expect(screen.getByRole('spinbutton')).toBeInTheDocument();
+  
+//   expect(screen.getByRole('button', {name: "Plot"})).toBeInTheDocument();
+  
+//   expect(screen.getByRole('button', {name: "Clear"})).toBeInTheDocument();
+// });
 
 
+test('presses the button', async () => {
+  
+  //Arrange
+  render(<CollatzConjecture />)
+    
+  //Act
+  userEvent.type(screen.getByRole('spinbutton'), '27');
+  userEvent.click(screen.getByRole('button', {name: "Plot"}));
+  // const operationsValue = screen.getByText(/operations:/i);
+  
+
+  // Assert
+  expect(screen.getByRole('spinbutton')).toHaveValue(27);
+
+})
